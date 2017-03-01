2d69686cd15efc4926fd6f40d116804b0b7d1fb1
Dung Nguyen <dung@letterink.co>
2017-03-01 14:49:25 +0700

    Update Install Bower component

---
+++ blueprints/ember-packery/index.js
@@ -0,0 +1,12 @@
+/*jshint node:true*/
+module.exports = {
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
+
+  afterInstall: function(options) {
+    return this.addBowerPackageToProject('packery');
+  }
+};
